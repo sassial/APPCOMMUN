24dff2143aedc9dccc3aaf7f48e53629af529d43
Alexandre Sassi <71190353+sassial@users.noreply.github.com>
2025-06-11 11:47:27 +0200

    maj

--- controleurs/capteurs.php
+++ controleurs/capteurs.php
@@ -1,20 +1,25 @@
 <?php
 
 // on inclut le fichier modèle contenant les appels à la BDD
-include('./modele/requetes.utilisateurs.php');
+include('./modele/requetes.capteurs.php');
 
 // si la fonction n'est pas définie, on choisit d'afficher l'accueil
 if (!isset($_GET['fonction']) || empty($_GET['fonction'])) {
-    $function = "accueil";
+    $function = "gestion";
 } else {
     $function = $_GET['fonction'];
 }
 
 switch ($function) {
     
-    case 'accueil':
+    case 'gestion':
         //affichage de l'accueil
-        $vue = "accueil";
+        $vue = "gestion";
+        break;
+    
+    case 'affichage':
+        //affichage de l'inscription
+        $vue = "affichage";
         break;
         
     default:
