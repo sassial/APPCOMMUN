bff17671b7fce818468b902962510469a025e25c
Alexandre Sassi <71190353+sassial@users.noreply.github.com>
2025-06-11 11:09:18 +0200

    maj controleur

--- index.php
+++ index.php
@@ -8,19 +8,15 @@
  * - Vue : contient ce qui doit être affiché
  **/
 
-// Activation des erreurs
 ini_set('display_errors', 1);
 
-// Appel des fonctions du contrôleur
 include("controleurs/fonctions.php");
-// Appel des fonctions liées à l'affichage
 include("vues/fonctions.php");
 
 // On identifie le contrôleur à appeler dont le nom est contenu dans cible passé en GET
 if(isset($_GET['cible']) && !empty($_GET['cible'])) {
     // Si la variable cible est passé en GET
     $url = $_GET['cible']; //user, sensor, etc.
-    
 } else {
     // Si aucun contrôleur défini en GET, on bascule sur utilisateurs
     $url = 'utilisateurs';
