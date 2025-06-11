a23545293f4797a0c8631e00331483842aefcb1d
Alexandre Sassi <71190353+sassial@users.noreply.github.com>
2025-06-11 12:03:27 +0200

    maj

---
+++ modele/requetes.utilisateurs.php
@@ -0,0 +1,49 @@
+<?php
+
+//on définit le nom de la table
+$table = "utilisateurs";
+
+// requêtes spécifiques à la table des capteurs
+
+
+/**
+ * Recherche un utilisateur en fonction du nom passé en paramètre
+ * @param PDO $bdd
+ * @param string $nom
+ * @return array
+ */
+function rechercheParNom(PDO $bdd, string $nom): array {
+    
+    $statement = $bdd->prepare('SELECT * FROM  utilisateurs WHERE nom = :nom');
+    $statement->bindParam(":nom", $value);
+    $statement->execute();
+    
+    return $statement->fetchAll();
+    
+}
+
+/**
+ * Récupère tous les enregistrements de la table utilisateurs
+ * @param PDO $bdd
+ * @return array
+ */
+function recupereTousUtilisateurs(PDO $bdd): array {
+    $query = 'SELECT * FROM utilisateurs';
+    return $bdd->query($query)->fetchAll();
+}
+
+/**
+ * Ajoute un nouvel utilisateur dans la base de données
+ * @param array $utilisateur
+ */
+function ajouteUtilisateur(PDO $bdd, array $utilisateur) {
+    
+    $query = ' INSERT INTO utilisateurs (nom, mot_de_passe) VALUES (:nom, :mot_de_passe)';
+    $donnees = $bdd->prepare($query);
+    $donnees->bindParam(":nom", $utilisateur['nom'], PDO::PARAM_STR);
+    $donnees->bindParam(":mot_de_passe", $utilisateur['mot_de_passe']);
+    return $donnees->execute();
+    
+}
+
+?>
