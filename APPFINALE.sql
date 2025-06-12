23780a1dd4eeedc07dc5cf6e8cb7f77884d05e24
Xilep05 <oscmillet@gmail.com>
2025-06-12 11:57:06 +0200

    maj

--- APPFINALE.sql
+++ APPFINALE.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jun 11, 2025 at 03:24 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Host: localhost:3306
+-- Generation Time: Jun 12, 2025 at 09:43 AM
+-- Server version: 5.7.24
+-- PHP Version: 8.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,8 +18,52 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `APPFINALE`
+-- Database: `appfinale`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dispositifs`
+--
+
+CREATE TABLE `dispositifs` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) NOT NULL,
+  `type` enum('capteur','actionneur') NOT NULL,
+  `nom_table_bdd` varchar(100) NOT NULL COMMENT 'Nom table BDD distante',
+  `unite` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `dispositifs`
+--
+
+INSERT INTO `dispositifs` (`id`, `nom`, `type`, `nom_table_bdd`, `unite`) VALUES
+(1, 'Température & Humidité', 'capteur', 'CapteurTempHum', '°C / %'),
+(2, 'Lumière', 'capteur', 'CapteurLumiere', 'lux'),
+(3, 'Proximité', 'capteur', 'CapteurProximite', 'cm'),
+(4, 'Gaz', 'capteur', 'CapteurGaz', 'ppm'),
+(5, 'Lumière Principale', 'actionneur', 'Lampe1', 'On/Off');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `etats_actionneurs`
+--
+
+CREATE TABLE `etats_actionneurs` (
+  `id_dispositif` int(11) NOT NULL,
+  `etat` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0=OFF, 1=ON',
+  `derniere_modif` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `etats_actionneurs`
+--
+
+INSERT INTO `etats_actionneurs` (`id_dispositif`, `etat`, `derniere_modif`) VALUES
+(5, 1, '2025-06-12 09:26:15');
 
 -- --------------------------------------------------------
 
@@ -32,21 +76,36 @@ CREATE TABLE `utilisateurs` (
   `prenom` varchar(100) DEFAULT NULL,
   `nom` varchar(100) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `password` varchar(255) DEFAULT NULL,
+  `role` varchar(50) NOT NULL DEFAULT 'utilisateur'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `utilisateurs`
 --
 
-INSERT INTO `utilisateurs` (`id`, `prenom`, `nom`, `email`, `password`) VALUES
-(1, 'Alexandre', 'Sassi', 'alex29.sassi@gmail.com', '$2y$10$tFqey/Ie7IZ75HHxjQjuOO0xIbHXEIP1kn3hdZftziP.rz.jJNkg.'),
-(3, 'S', 'O', 'os@gmail.com', '$2y$10$8nTxKdp/dsZcm2A6DbRFBeoJcox9Z0T025DTIgX6OaYmiFDMFJ7ki'),
-(4, 'SA', 'OS', 'ossa@gmail.com', '$2y$10$3Srxf2wO/dYyydSChLdUPuRdYdh/Hr9oUv08btE04wwFMDyiVFW02');
+INSERT INTO `utilisateurs` (`id`, `prenom`, `nom`, `email`, `password`, `role`) VALUES
+(1, 'Alexandre', 'Sassi', 'alex29.sassi@gmail.com', '$2y$10$tFqey/Ie7IZ75HHxjQjuOO0xIbHXEIP1kn3hdZftziP.rz.jJNkg.', 'utilisateur'),
+(3, 'S', 'O', 'os@gmail.com', '$2y$10$8nTxKdp/dsZcm2A6DbRFBeoJcox9Z0T025DTIgX6OaYmiFDMFJ7ki', 'utilisateur'),
+(4, 'SA', 'OS', 'ossa@gmail.com', '$2y$10$3Srxf2wO/dYyydSChLdUPuRdYdh/Hr9oUv08btE04wwFMDyiVFW02', 'utilisateur'),
+(5, 'Ruben', 'Legrandjacques', 'legrandjacques.ruben@gmail.com', '$2y$10$J4vLHX6QWPB8QFdCE0fCVOJRVykI3J1EkK.bk5aKq6jv08pSpOqVq', 'utilisateur'),
+(6, 'Ruben', 'Legrandjacques', 'rlegrandjacques@juniorisep.com', '$2y$10$C/NAgRdIL7getjavTGzpvOxyJJozQYuh3O5AM.TbX4XS/EpzpdQL2', 'utilisateur');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `dispositifs`
+--
+ALTER TABLE `dispositifs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `etats_actionneurs`
+--
+ALTER TABLE `etats_actionneurs`
+  ADD PRIMARY KEY (`id_dispositif`);
 
 --
 -- Indexes for table `utilisateurs`
@@ -59,10 +118,16 @@ ALTER TABLE `utilisateurs`
 --
 
 --
+-- AUTO_INCREMENT for table `dispositifs`
+--
+ALTER TABLE `dispositifs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `utilisateurs`
 --
 ALTER TABLE `utilisateurs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
